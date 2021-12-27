c388d5de7ff31d21d3207c8d0d224c7eb6b7da21
coder9a <rajsinghbalhara@gmail.com>
2021-12-27 23:43:50 +0530

    new commit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
 
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
--- home/views.py
+++ home/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
 
-def contact(request):
-    return render(request, 'home/contact.html')
-
 def about(request):
+    messages.success(request, 'Welcome to About')
     return render(request, 'home/about.html')
+
+def contact(request):
+    messages.success(request, 'Welcome to Contact')
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        # print(name, email, phone, content)
+        if len(name)<2 or len(email)<2 or len(phone)<10 or len(content)<2:
+            messages.error(request, "Please fill the form correctly")
+        else:
+            contact = Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, 'Your form has been sent successfully!')
+    return render(request, 'home/contact.html')
